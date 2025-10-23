438f6f068cb3628445c515815d3e49caa844f2e3
AkilanChithra <chithra.sathish.akilan@gmail.com>
2025-10-23 16:50:39 -0500

    added the looping mechanism for interactive functionality

--- main.rkt
+++ main.rkt
@@ -1,1 +1,26 @@
 #lang racket
+
+
+(include "mode.rkt")
+
+
+(define interactive? prompt?)
+
+
+(define (repl-loop history)
+  (when interactive?
+    (display "> ")
+    (flush-output))                
+
+  (define line (read-line))
+  (cond
+    [(eof-object? line) (void)]     
+    [(string=? line "quit") (void)] 
+    [else
+     
+     (when interactive?
+       (displayln (string-append "echo: " line)))
+     (repl-loop history)]))
+
+(module+ main
+  (repl-loop '()))    
